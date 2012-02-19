70f9c248959008c46b6ae2801c9cdb3117a6951a
Mathew Says IdareYou <sportcucs@hotmail.com>
2012-02-19 15:55:33 +0800

    Taken from gio

---
+++ include/generated/compile.h
@@ -0,0 +1,9 @@
+/* This file is auto generated, version 780 */
+/*  PREEMPT */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#780 PREEMPT Sun Jan 22 03:24:23 WIT 2012"
+#define LINUX_COMPILE_TIME "03:24:23"
+#define LINUX_COMPILE_BY "dhiika"
+#define LINUX_COMPILE_HOST "phiexz"
+#define LINUX_COMPILE_DOMAIN
+#define LINUX_COMPILER "gcc version 4.5.2 (Sourcery G++ Lite 2011.03-42) "
